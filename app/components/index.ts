78e9bbb8ddfe821da0ef97c697caaf73acc005c6
Javier Santoyo <javier.santoyo@yahoo.com>
2023-07-14 00:42:45 -0600

    Update barrel of components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Card } from './Card/Card';
 export { default as Footer } from './Footer/Footer';
-export * from './Header';
+export { default as Header } from './Header/Header';
 export { default as loading } from './loading';
-export { default as Navbar } from './Header/Header';
