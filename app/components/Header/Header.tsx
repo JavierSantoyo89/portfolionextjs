1006704cd1968740f2617aebc7ad9bfda7b74c26
Javier Santoyo <javier.santoyo@yahoo.com>
2023-07-14 17:18:15 -0600

    Fix: Style of Header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,10 +1,5 @@
-"use client"
 import Link from "next/link"
-import "@theme-toggles/react/css/Within.css"
-import { Within } from "@theme-toggles/react"
 import "flowbite"
-
-/**  <Within duration={900} title="Toggle theme" style={{ fontSize: 60 }} /> */
 
 function Header() {
   return (
@@ -15,7 +10,9 @@ function Header() {
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
-              className="inline-flex items-center justify-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center justify-center p-2 ml-1 text-sm text-gray-500 
+              rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 
+              dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="mobile-menu-2"
               aria-expanded="false"
             >
@@ -54,14 +51,10 @@ function Header() {
               <li>
                 <Link
                   href="/"
-                  className="block py-2 pr-4 pl-3 
-                  text-gray-700 border-b
-                   border-gray-100 
-                   hover:bg-blue-700 
-                   lg:bg-transparent 
-                   lg:text-blue-700 lg:p-0
-                    dark:text-white"
-                  aria-current="page"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100  
+                  hover:bg-blue-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 
+                  lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 
+                  dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Home
                 </Link>
@@ -69,7 +62,10 @@ function Header() {
               <li>
                 <Link
                   href="/portfolio"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100  hover:bg-blue-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100  
+                  hover:bg-blue-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 
+                  lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 
+                  dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Portafolio
                 </Link>
