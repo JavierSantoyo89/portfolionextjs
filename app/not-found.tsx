747747c038ee0c9fdfea874afbc6a99a39c16506
Javier Santoyo <javier.santoyo@yahoo.com>
2023-07-15 00:41:35 -0600

    Style: Refactoring Style of "NotFound"

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,6 +4,9 @@
 import Image from "next/image"
 import space404 from "../public/Error404.png" //* === Imagen de fondo === *//
 
+{
+  /* <Image src={space404} alt="background image" fill /> */
+}
 function notFound() {
   //* --------- Funcion para regresar un paso en el historial de navegacion mediante el "onClick" --------- */
   function back() {
@@ -11,10 +14,8 @@ function notFound() {
   }
   //* ============================================================================== *//
   return (
-    <div className="relative h-screen" onClick={back}>
-      <div className="absolute inset-0">
-        <Image src={space404} alt="background image" fill />
-      </div>
+    <div className="w-full h-full" onClick={back}>
+      <Image src={space404} alt="background image" fill />
     </div>
   )
 }
