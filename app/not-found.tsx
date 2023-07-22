49607e790a8bd725661e70b89fa4ee36a8d99e2c
Javier Santoyo <javier.santoyo@yahoo.com>
2023-07-22 15:36:00 -0600

    Style: Update CSS of "404 error"

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 "use client"
 //* =========== Cuando no encuentra el sitio solicitado se abre "Not Found" ======== *//
 import Image from "next/image"
-import space404 from "../public/Error404.png" //* === Imagen de fondo === *//
+import space404 from "../public/404Error.png" //* === Imagen de fondo === *//
 
 {
   /* <Image src={space404} alt="background image" fill /> */
@@ -15,14 +15,25 @@ function NotFound() {
   return (
     <body
       onClick={back}
-      className=" flex flex-row bg-black bbg-cover bg-no-repeat bg-center "
+      // // 
+      // className="flex flex-auto w-screen h-screen overflow-y-scroll  bg-black flex-col justify-center items-center  bg-404Error  bg-no-repeat bg-contain"
+    
+      className="flex w-screen h-screen overflow-y-scroll  bg-black  justify-center items-center  bg-404Error bg-center bg-no-repeat bg-contain"
     >
-      <Image  
+      {/* <Image  
       src={space404} 
       alt="background image error 404"
       className="relative"
       sizes="(max-width: 768px) 100vw"
-      quality={20} />
+      quality={20} /> */}
+      <section>
+        <p className="bg-[#dcdf34] px-2 text-sm rounded rotate-12  ">
+          404 Error, Page Not Found
+        </p>
+        <p className="bg-[#dcdf34] px-2 text-sm rounded rotate-12 absolute  text-center  mt-2                ">
+          User, we have a problem!!
+        </p>
+      </section>
     </body>
   )
 }
