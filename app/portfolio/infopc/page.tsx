a96c04cddcee016ef45c1ec4652b578c088705f4
Javier Santoyo <javier.santoyo@yahoo.com>
2023-07-25 15:17:15 -0600

    Fix: Delete MAC & IP

--- app/portfolio/infopc/page.tsx
+++ app/portfolio/infopc/page.tsx
@@ -8,6 +8,7 @@ import {
   type,
   networkInterfaces,
 } from "node:os"
+
 // console.log("Informacion del equipo")
 // console.log("----------------------")
 // console.log("Nombre del sistema operativo", os.platform())
@@ -17,12 +18,16 @@ import {
 // console.log("Memoria total", os.totalmem() / 1024 / 1024)
 
 function page() {
-  const SO = platform()
-  const vSO = release()
-  const Arch = arch()
-  const memFree = Math.trunc(freemem() / 1024 / 1024).toFixed(0)
-  const memTot = Math.round(totalmem() / 1024 / 1024 / 1024).toFixed(2)
-  const porcentFree = Math.trunc((memFree / 1024 / memTot) * 100)
+  const SO: String = platform()
+  const vSO: String = release()
+  const Arch: String = arch()
+  const memoryTot = totalmem()
+  const freememory = freemem()
+  const memFree = Math.floor(freememory / 1024 / 1024).toFixed(0)
+  const memTot: string = Math.round(memoryTot / 1024 / 1024 / 1024).toFixed(2)
+  const porcentFree = Math.trunc(
+    (Math.floor(parseInt(memFree)) / 1024 / parseInt(memTot)) * 100
+  )
   const upTime = uptime()
   const tipoOS = type()
   const ethernetData = networkInterfaces()
@@ -35,6 +40,11 @@ function page() {
   const minutos = Math.floor(minutosglobales % 60)
   //* =================================================================================== //
   // const arrayInfo = [SO, vSO, dias, horas, minutos, Arch, memFree, memTot] // Array de practica
+  
+
+  // let MAC:string = ethernetData["Ethernet"]?[0]?["mac"]
+  // const IP:string = ethernetData["Ethernet"]?[3]?["address"]
+  
 
   //* --------------------- console.log para revisar en la terminal -------------------- *//
   // console.log("El arrey nuevo es: ", Array)
@@ -68,8 +78,8 @@ function page() {
         <p>Tipo de sistema operativo: {tipoOS}</p>
         <p>Version: {vSO}</p>
         <p>Arquitectura: {Arch}</p>
-        <p>MAC Address: {ethernetData["Ethernet"][0]["mac"]}</p>
-        <p>Tu IP: {ethernetData["Ethernet"][3]["address"]}</p>
+        {/* <p>MAC Address: {MAC}</p> */}
+        {/* <p>Tu IP: {IP}</p> */}
         <p>
           Tiempo sin reiniciar el equipo: {dias}:{horas}:{minutos} (dd/hh/mm)
         </p>
