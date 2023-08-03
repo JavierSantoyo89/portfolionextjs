aece283e0c266092ca037878fbbc13b527d389da
Javier Santoyo <javier.santoyo@yahoo.com>
2023-08-03 14:54:54 -0600

    Fix: footer, create img components

---
+++ app/components/icons/Twitter/TwitterIcon.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  width?: number
+  height?: number
+}
+
+function Twitter({ width = 40, height = 40 }: Props) {
+  return (
+    <svg
+      version="1.1"
+      // id="svg5"
+      xmlns="http://www.w3.org/2000/svg"
+      // x="0px"
+      // y="0px"
+      width={width}
+      hanging={height}
+      viewBox="100 -100 1600.56 1221.19"
+      className="fill-current"
+    >
+      <g id="">
+        <path d="M283.94,167.31l386.39,516.64L281.5,1104h87.51l340.42-367.76L984.48,1104h297.8L874.15,558.3l361.92-390.99   h-87.51l-313.51,338.7l-253.31-338.7H283.94z M412.63,231.77h136.81l604.13,807.76h-136.81L412.63,231.77z" />
+      </g>
+    </svg>
+  )
+}
+
+export default Twitter
---
+++ app/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Card } from './Card/Card';
+export { default as Footer } from './Footer/Footer';
+export { default as error } from './error';
+export { default as head } from './head/head';
+export { default as GithubIcon } from './icons/githubIcon/GithubIcon';
+export * from './icons/Linkedin/LinkendinIcon';
+export { default as TwitterIcon } from './icons/Twitter/TwitterIcon';
+export * from './index';
