888b62970f009729dc59f2ebb46c15044e04b073
Javier Santoyo <javier.santoyo@yahoo.com>
2023-08-08 01:31:12 -0600

    Style: Fix bug with tailwind of position.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,30 +1,34 @@
-
 "use client"
 //* =========== Cuando no encuentra el sitio solicitado se abre "Not Found" ======== *//
 function NotFound() {
   //* --------- Funcion para regresar un paso en el historial de navegacion mediante el "onClick" --------- */
   function back() {
     window.history.back()
-
   }
   //* ============================================================================== *//
   return (
-    <body
-    
-    onClick={back}
-    // className="flex flex-auto w-screen h-screen overflow-y-scroll  bg-black flex-col justify-center items-center  bg-404Error  bg-no-repeat bg-contain"
-    className="flex w-screen h-screen overflow-y-scroll  bg-black  justify-center items-center  bg-404Error bg-center bg-no-repeat bg-contain"
+    <main
+      onClick={back}
+      // className="flex flex-auto w-screen h-screen overflow-y-scroll  bg-black flex-col justify-center items-center  bg-404Error  bg-no-repeat bg-contain"
+      className="
+      flex flex-col
+      w-screen sm:h-[100vh] md:h-[100vh] lg:h-[100vh]  my-auto
+      justify-center items-center overflow-visible
+  bg-black     
+    bg-404Error 
+    bg-center 
+    bg-no-repeat 
+    bg-contain"
     >
-
-      <section>
-        <p className="bg-[#dcdf34] px-2 text-sm rounded rotate-12  ">
+      <section className=" m-auto ">
+        <p className="bg-[#da7024] px-2 text-sm rounded rotate-12  ">
           404 Error, Page Not Found
         </p>
-        <p className="bg-[#dcdf34] px-2 text-sm rounded rotate-12 absolute  text-center  mt-2                ">
+        <p className="bg-[#da7024] px-2 text-sm rounded rotate-12  mt-2  ">
           User, we have a problem!!
         </p>
       </section>
-    </body>
+    </main>
   )
 }
 
