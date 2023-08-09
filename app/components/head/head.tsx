bc10b01c1226e09a18ef9fa6fc4d8f2dc96d5daf
Javier Santoyo <javier.santoyo@yahoo.com>
2023-08-09 15:32:33 -0600

    Fix: bug in head (Responsive)

--- app/components/head/head.tsx
+++ app/components/head/head.tsx
@@ -2,97 +2,42 @@
 import Link from "next/link"
 import "flowbite"
 import ThemeButton from "./ThemeButton/ThemeSwitcher.jsx"
+import Image from "next/image.js"
 
 export default function Head() {
   return (
     <header>
-      <nav className="bg-gray-200 relative px-4 lg:px-6 py-2.5 dark:bg-blue-800">
-        <div className="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl">
-          <div className="flex items-center lg:order-2">
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              className="inline-flex items-center justify-center p-2 ml-1 text-sm text-gray-500 
-          rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 
-          dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <svg
-                className="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div
-            className="hidden justify-center items-center w-full lg:flex lg:w-auto lg:order-1"
-            id="mobile-menu-2"
+      {/* <!-- component --> */}
+      <nav className="bg-white shadow dark:bg-gray-800">
+        <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+          <Link
+            href="/"
+            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
-                <Link
-                  href="/"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100  
-              hover:bg-blue-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 
-              lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 
-              dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/portfolio"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100  
-              hover:bg-blue-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 
-              lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 
-              dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Portafolio
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/about"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-blue-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Sobre mi
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/contact"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100  hover:bg-blue-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Contacto
-                </Link>
-              </li>
-              <li>
-                <ThemeButton />
-              </li>
-            </ul>
-          </div>
+            Home
+          </Link>
+          <Link
+            href="/portfolio"
+            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+          >
+            Portafolio
+          </Link>
+          <Link
+            href="/about"
+            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+          >
+            Sobre mi
+          </Link>
+          <Link
+            href="/contact"
+            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+          >
+            Contacto
+          </Link>
+
+          
+
+          <ThemeButton />
         </div>
       </nav>
     </header>
