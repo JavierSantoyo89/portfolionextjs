9adac5772569ef1927f9ef11eee3e346a535b5b9
Javier Santoyo <javier.santoyo@yahoo.com>
2023-08-23 18:14:00 -0600

    Edit image in 'portfolio'

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,129 +1,125 @@
-
-import Link from "next/link"
-import Image from "next/Image"
-import CurAME from "../public/curame.webp"
-import RickAndMort from "../public/rickAndMortyAPI.jpeg"
+import Link from "next/link";
+import Image from "next/Image";
+import CurAME from "../public/curame.webp";
+import RickAndMort from "../public/rickAndMortyAPI.jpeg";
 
 function Portfolio() {
   return (
     <div>
-       {/* <!-- component --> */}
-       <div className="h-screen flex items-center justify-center">
-         <div className="grid grid-cols-12 gap-2 gap-y-4 max-w-6xl">
-           {/* <!-- Video 1 --> */}
-           <div className="col-span-12 sm:col-span-6 md:col-span-3">
-             <div className="w-full flex flex-col">
-               <div className="relative">
-                 {/* <!-- Image Video --> */}
-                 <Link href="https://www.curame.org.mx/">
-                   <Image
-                     src={CurAME}
-                     alt="Logo CurAME Mexico"
+      {/* <!-- component --> */}
+      <div className="h-screen flex items-center justify-center">
+        <div className="grid grid-cols-12 gap-2 gap-y-4 max-w-6xl">
+          {/* <!-- Video 1 --> */}
+          <div className="col-span-12 sm:col-span-6 md:col-span-3">
+            <div className="w-full flex flex-col">
+              <div className="relative">
+                {/* <!-- Image Video --> */}
+                <Link href="https://www.curame.org.mx/">
+                  <Image
+                    src={CurAME}
+                    alt="Logo CurAME Mexico"
                     className="w-96 h-auto"
-                   />
-                 </Link>
-                 <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
-                   Wordpress
-                 </p>
-               </div>
+                  ></Image>
+                </Link>
+                <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
+                  Wordpress
+                </p>
+              </div>
 
-               <div className="flex flex-row mt-2 gap-2">
-                 {/* <!-- Profile Picture --> */}
-                 <Link href="https://www.curame.org.mx/">
-                   <Image
-                     src={CurAME}
-                     alt="Lgo CurAME Mexico"
-                     width={40}
-                     height={40}
-                     className="rounded-full max-h-10 max-w-10"
-                   />
-                 </Link>
+              <div className="flex flex-row mt-2 gap-2">
+                {/* <!-- Profile Picture --> */}
+                <Link href="https://www.curame.org.mx/">
+                  <Image
+                    src={CurAME}
+                    alt="Lgo CurAME Mexico"
+                    width={40}
+                    height={40}
+                    className="rounded-full max-h-10 max-w-10"
+                  ></Image>
+                </Link>
 
-                 {/* <!-- Description --> */}
-                 <div className="flex flex-col">
+                {/* <!-- Description --> */}
+                <div className="flex flex-col">
                   <Link href="#">
-                     <p className="text-gray-100 text-sm font-semibold">
-                       CurAME Mexico A.C.
-                     </p>
-                   </Link>
-                   <Link
-                     className="text-gray-400 text-xs mt-2 hover:text-gray-100"
-                     href="#"
-                   >
-                     Web diseñada para la organizacion civil `Atrofia Muscular
-                     Espinal en Mexico`
-                   </Link>
-                   <p className="text-gray-400 text-xs mt-1">
-                     241K views . 3 years ago
-                   </p>
-                 </div>
-               </div>
-             </div>
-           </div>
-
-           {/* <!-- Video 2 --> */}
-           <div className="col-span-12 sm:col-span-6 md:col-span-3">
-             <div className="w-full flex flex-col">
-               <div className="relative">
-                 {/* <!-- Image Video --> */}
-                 <Link href="/portfolio/rickandmorty">
-                   <Image
-                     src={RickAndMort}
-                     alt="Sitio de rick and morty"
-                     width={90}
-                     height={40}
-                     className="w-96 h-auto"
-                   />
-                 </Link>
-
-                               </div>
-
-               <div className="flex flex-row mt-2 gap-2">
-                 {/* <!-- Profile Picture --> */}
-                 <Link href="/portfolio/rickandmorty">
-                   <Image
-                     src={RickAndMort}
-                     alt="Sitio random 2"
-                     width={40}
-                     height={40}
-                     className="rounded-full max-h-10 max-w-10"
-                   />
-                 </Link>
-
-                 {/* <!-- Description --> */}
-                 <div className="flex flex-col">
-                   <Link href="/portfolio/rickandmorty">
-                     <p className="text-gray-100 text-sm font-semibold">
-                       Rick & morty practica
-                     </p>
-                   </Link>
-                   <Link
+                    <p className="text-gray-100 text-sm font-semibold">
+                      CurAME Mexico A.C.
+                    </p>
+                  </Link>
+                  <Link
                     className="text-gray-400 text-xs mt-2 hover:text-gray-100"
                     href="#"
                   >
-                  Practica de conexion con un servicio externo
+                    Web diseñada para la organizacion civil `Atrofia Muscular
+                    Espinal en Mexico`
                   </Link>
-                  
+                  <p className="text-gray-400 text-xs mt-1">
+                    241K views . 3 years ago
+                  </p>
                 </div>
               </div>
             </div>
           </div>
 
-           {/* <!-- Video 3 --> */}
-           <div className="col-span-12 sm:col-span-6 md:col-span-3">
-             <div className="w-full flex flex-col">
-               <div className="relative">
-               {/* <!-- Image Video --> */}
+          {/* <!-- Video 2 --> */}
+          <div className="col-span-12 sm:col-span-6 md:col-span-3">
+            <div className="w-full flex flex-col">
+              <div className="relative">
+                {/* <!-- Image Video --> */}
+                <Link href="/portfolio/rickandmorty">
+                  <Image
+                    src={RickAndMort}
+                    alt="Sitio de rick and morty"
+                    width={90}
+                    height={40}
+                    className="w-96 h-auto"
+                  ></Image>
+                </Link>
+              </div>
+
+              <div className="flex flex-row mt-2 gap-2">
+                {/* <!-- Profile Picture --> */}
+                <Link href="/portfolio/rickandmorty">
+                  <Image
+                    src={RickAndMort}
+                    alt="Sitio random 2"
+                    width={40}
+                    height={40}
+                    className="rounded-full max-h-10 max-w-10"
+                  ></Image>
+                </Link>
+
+                {/* <!-- Description --> */}
+                <div className="flex flex-col">
+                  <Link href="/portfolio/rickandmorty">
+                    <p className="text-gray-100 text-sm font-semibold">
+                      Rick & morty practica
+                    </p>
+                  </Link>
+                  <Link
+                    className="text-gray-400 text-xs mt-2 hover:text-gray-100"
+                    href="#"
+                  >
+                    Practica de conexion con un servicio externo
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* <!-- Video 3 --> */}
+          <div className="col-span-12 sm:col-span-6 md:col-span-3">
+            <div className="w-full flex flex-col">
+              <div className="relative">
+                {/* <!-- Image Video --> */}
                 <Link href="/portfolio/infopc">
-    <Image
+                  <Image
                     src={CurAME}
                     alt="Sitio donde se muestran estadisticas del servicio de frontend y backend"
                     width={40}
                     height={40}
                     className="w-96 h-auto"
-                  />
+                  ></Image>
                 </Link>
-
               </div>
 
               <div className="flex flex-row mt-2 gap-2">
@@ -135,14 +131,14 @@ function Portfolio() {
                     width={40}
                     height={40}
                     className="rounded-full max-h-10 max-w-10"
-                  />
+                  ></Image>
                 </Link>
 
                 {/* <!-- Description --> */}
                 <div className="flex flex-col">
                   <Link href="#">
                     <p className="text-gray-100 text-sm font-semibold">
-                    Estadisticas del servicio de frontend y backend
+                      Estadisticas del servicio de frontend y backend
                     </p>
                   </Link>
                 </div>
@@ -161,7 +157,7 @@ function Portfolio() {
                     width={40}
                     height={40}
                     className="w-96 h-auto"
-                  />
+                  ></Image>
                 </Link>
 
                 <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
@@ -178,7 +174,7 @@ function Portfolio() {
                     width={40}
                     height={40}
                     className="rounded-full max-h-10 max-w-10"
-                  />
+                  ></Image>
                 </Link>
 
                 {/* <!-- Description --> */}
@@ -196,9 +192,6 @@ function Portfolio() {
                     {" "}
                     Web Dev Simplified{" "}
                   </Link>
-                  <p className="text-gray-400 text-xs mt-1">
-                    241K views . 3 years ago
-                  </p>
                 </div>
               </div>
             </div>
@@ -206,12 +199,12 @@ function Portfolio() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
 
 export const metadata = {
   title: "Portfolio",
   description: "Descripcion de proyectos realizados",
-}
+};
