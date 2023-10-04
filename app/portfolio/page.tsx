30c94f78d608a2e95039081172fd1498667c2d89
Javier Santoyo <javier.santoyo@yahoo.com>
2023-10-04 01:39:06 -0600

    Edit Css of portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,25 +5,32 @@ import img1 from "../public/CurAMEMexico.png";
 import img2 from "../../public/loginHtml.png";
 import img3 from "../public/rickAndMortyAPI.jpeg";
 import { useState } from "react";
+import Link from "next/link";
 
 const cards = [
   {
     id: 1,
     header: "CurAME",
     image: img1,
+    link:"www.curame.org.mx",
     text: "Diseño creado para una ONG",
+    Description:"Primer proyecto usando wordpress, revisando el contenido necesario para mostrar con el cliente y editando algunas imágenes para poder ser ilustrativas para la ONG junto con la creación de 'política de privacidad' y de diseño básico de base de datos.",
   },
   {
     id: 2,
     header: "Rick&Morty",
     image: img3,
+    link:"portfolio/rickandmorty",  
     text: "Practica usando llamado a API's externas",
+    Description: "Practica de cómo usar información mediante API’s externas(get) y manipulando la información a mostrar al usuario."
   },
   {
     id: 3,
     header: "Login",
     image: img2,
+    link:"portfolio/login",
     text: "Practica de diseño usando typescript y CSS puro",
+    description:"Prueba de diseño en cuanto interfas de usuario usando HTML, CSS , Javascript"
   },
 ];
 
@@ -32,28 +39,29 @@ function Portfolio() {
   const handleToggle = (index: number) => setActive(index);
   return (
     <main className=" grid place-items-center ">
-
-     <section >
-      {cards.map((card, index) => {
-        const isActive = active === index ? "active" : "";
-        return (
-          <article
-          key={card.id}
-          className={isActive}
-          onClick={() => handleToggle(index)}
-          >
-            <Image src={card.image} alt={card.text} className="h-auto"/>
-            <div className="content">
-              <div>
-                <h2>{card.header}</h2>
-                <p>{card.text}</p>
+      <section>
+        {cards.map((card, index) => {
+          const isActive = active === index ? "active" : "";
+         
+          return (
+            <article
+              key={card.id}
+              className={isActive}
+              onClick={() => handleToggle(index)}
+            >
+              <Image src={card.image} alt={card.text} className="h-auto" />
+              <div className="content">
+                <div>
+                  <h2><Link href={card.link}>{card.header}</Link> </h2>
+                  <p>{card.text}</p>
+                </div>
               </div>
-            </div>
-          </article>
-        );
-      })}
-    </section> 
-</main>
+            </article>
+          );
+        })}
+      </section>
+      
+    </main>
   );
 }
 
