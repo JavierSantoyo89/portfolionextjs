4dff92aa6fd1fe71ba8d19c5480d8da749726d81
Javier santoyo <javier.santoyo@yahoo.com>
2023-10-12 23:58:21 -0600

    style: refactoring CSS fron Login.tsx

--- app/portfolio/login/page.tsx
+++ app/portfolio/login/page.tsx
@@ -1,64 +1,80 @@
-'use client'
+"use client";
 import { useState } from "react";
-import Link from "next/link";
-import './login.css'
+import "./login.css";
 function page() {
-// const [singInActive, setSingInActive] = useState(0)
-  const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+  const signUpButton = document.getElementById("signUp");
+  const signInButton = document.getElementById("signIn");
+  const container = document.getElementById("login-container");
+  function cambioSlide() {
+    console.log("jalo");
+  }
 
-// signUpButton.addEventListener('click', () => {
-//     console.log("entro en el sign up");
-//   container.classList.add('right-panel-active');
-// });
+  // const [singInActive, setSingInActive] = useState(0)
 
-// signInButton.addEventListener('click', () => {
-//     console.log('entro en el sign in');
-//   container.classList.remove('right-panel-active');
-// });
+  // signUpButton.addEventListener('click', () => {
+  //     console.log("entro en el sign up");
+  //   container.classList.add('right-panel-active');
+  // });
+
+  // signInButton.addEventListener('click', () => {
+  //     console.log('entro en el sign in');
+  //   container.classList.remove('right-panel-active');
+  // });
 
   return (
-  
-    <main className='main-login'>
-      <div className='container-login' id='container-login'>
-        <div className='sign-up-container-login form=container-login'>
-          <form id='form-login' action="#">
-            <h1 className='title-login'>Create your account</h1>
-            <span className='spanText'>Insert you email for registration</span>
-            <input id='input-login' type="text" placeholder="Name"></input>
-            <input id='input-login' type="email" placeholder="Email"></input>
-            <input id='input-login' type="password" placeholder="Password"></input>
+    <main className="main-login">
+      <div className="login-container" id="login-container">
+        <div className="login-form-container login-sign-up-container">
+          <form id="login-form" action="#">
+            <h1 className="login-h1">Create your account</h1>
+            <span className="login-span">Insert you email for registration</span>
+            <input id="login-input" type="text" placeholder="Name"></input>
+            <input id="login-input" type="email" placeholder="Email"></input>
+            <input
+              id="login-input"
+              type="password"
+              placeholder="Password"
+            ></input>
             <br />
           </form>
         </div>
-        <div className="form-container-login sign-in-container-login">
-          <form id='form-login' action="#">
-            <h1 className='title-login'>Log in</h1>
-            <span className='spanText'>Login to your account</span>
-            <input id='input-login' type="email" placeholder="Email"></input>
-            <input id='input-login' type="password" placeholder="Password"></input>
-            <Link className='linkForgot' href={'#'}>Forgot your password?</Link>
-            <button className='button-login'>Sign In</button>
+        <div className="login-form-container login-sign-in-container">
+          <form id="login-form" action="#">
+            <h1 className="login-h1">Log in</h1>
+            <span className="login-span">Login to your account</span>
+            <input id="login-input" type="email" placeholder="Email"></input>
+            <input
+              id="login-input"
+              type="password"
+              placeholder="Password"
+            ></input>
+            <a id="login-link" href="#">
+              Forgot your password?
+            </a>
+            <button id="login-button" className="login-button">
+              Sign In
+            </button>
           </form>
         </div>
-        <div className='overlayContainer-login'>
-          <div className="overlay">
-            <div>
-            {/* <div className={style.overlayPanel} className ={ style.overlayLeft}> */}
-              <h1 className='title-login'>Have already Account?</h1>
-              <p className='paragraph'>
+        <div className="login-overlay-container">
+          <div className="login-overlay">
+            <div className="login-overlay-panel login-overlay-left">
+              <h1 className="login-h1">Have already Account?</h1>
+              <p id="login-p">
                 To keep connect with us please login with your account here!
               </p>
-              <button className='ghost' id="signIn">
+              <button className="ghost login-button" id="signIn">
                 Sign in
               </button>
             </div>
-            
-            <div className='overlayPanel overlayRight'>
-              <h1 className='title-login'>Create your account</h1>
-              <p className='paragraph'>Enter your personal detail and start journey with us</p>
-              <button className='ghost' id="signUp">
+            <div className="login-overlay-panel login-overlay-right">
+              <h1 className="login-h1">Create your account</h1>
+              <p id="login-p">Enter your personal detail and start journey with us</p>
+              <button
+                className="ghost login-button"
+                id="signUp"
+                onClick={cambioSlide}
+              >
                 Sign up
               </button>
             </div>
