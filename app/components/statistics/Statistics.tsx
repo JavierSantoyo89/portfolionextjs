697eea291f13638775e4ebda3f68ee3660ae7c7d
JavierSantoyo89 <javier.santoyo@yahoo.com>
2026-01-31 01:25:01 -0600

    disable stadistics

--- app/components/statistics/Statistics.tsx
+++ app/components/statistics/Statistics.tsx
@@ -10,18 +10,18 @@ function Statistics({ sizeStadistics = 800 }: Props) {
       <h2>Estadisticas en Github</h2>
 
       <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-2 p-3   ">
-        <section className="m-5">
+        {/* <section className="m-5">
         <iframe src= "https://github-readme-stats.vercel.app/api/top-langs/?username=javiersantoyo89&layout=compact&theme=algolia&hide_border=true&text_color=ffffff"
 height='165px'
 width='300px'
 ></iframe>
-        </section>
+        </section> */}
 
-        <section className="m-5">
+        {/* <section className="m-5">
         <iframe src="https://github-readme-stats.vercel.app/api?username=JavierSantoyo89&show_icons=true&theme=algolia&hide_border=true&text_color=ffffff"
         height="195px"
         width="468px" ></iframe>
-        </section>
+        </section> */}
       </div>
     </div>
   );
